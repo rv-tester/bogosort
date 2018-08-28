2a093bffae4214dc13f61b161d53d27f1e76667e
Yiyi Wang <shd101wyy@sina.com>
2018-08-28 10:45:30 -0500

    Fix rv.sh

--- rv.sh
+++ rv.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 kcc -fissue-report=my_errors.json main.c -o bogosort
 echo "9 43 27 12 5 9 3" | ./bogosort
-rv-html-report my_errors.json -o report
+touch my_errors.json && rv-html-report my_errors.json -o report
 rv-upload-report `pwd`/report
